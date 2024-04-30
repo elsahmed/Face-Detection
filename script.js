d0359ac7b11c14013b049dc43bca37be3d4f570b
Elsa <imelsahmed@gmail.com>
2024-04-29 21:59:02 -0400

    centred canvas to video without offset

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-const video = document.getElementById('video')
+const video = document.getElementById('video') // gets video element
+const div = document.getElementById('div') // gets div element that holds video elememt 
 
+// gets the x and y coordinates of the video
+const video_x = video.getBoundingClientRect().left; 
+const video_y= video.getBoundingClientRect().top;
+
+document.getElementById("xy-cord").innerHTML= video_x + ' x ' + video_y;
+
+// loads all the faceapi models
 Promise.all([
     faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
     faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
@@ -7,7 +15,7 @@ Promise.all([
     faceapi.nets.faceExpressionNet.loadFromUri('/models')
   ]).then(startVideo)
 
-
+// start the video and send error is cannot start
 function startVideo() {
     navigator.getUserMedia(
         { video: {} },
@@ -16,14 +24,11 @@ function startVideo() {
     )
 }
 
+// when video starts follow
 video.addEventListener('play', () => {
     const canvas = faceapi.createCanvasFromMedia(video)
-
-    canvas.style.position = 'absolute'; // Set the position to absolute to control its position explicitly
-    canvas.style.top = `80px`; // Set the new Y-coordinate
-    canvas.style.left = `360px`; // Set the new X-coordinate
-    
-    document.body.append(canvas)
+    // document.body.append(canvas)
+    div.append(canvas)
     const displaySize = { width: video.width, height: video.height }
     faceapi.matchDimensions(canvas, displaySize)
 
